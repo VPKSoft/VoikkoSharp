4a594dd303602400e571617951d2ef6e907124c0
VPKSoft <vpksoft@vpksoft.net>
2020-12-17 18:47:51 +0200

    Added NET Core support for the package.

--- libvoikko/Properties/AssemblyInfo.cs
+++ libvoikko/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("libvoikko.Properties")]
-[assembly: AssemblyDescription("Voikko is a morphological analyzer, spelling and grammar checker, hyphenator and collection of related linguistic data for Finnish language.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Voikko")]
-[assembly: AssemblyProduct("Voikko")]
-[assembly: AssemblyCopyright("Copyright © Voikko 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,13 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0f3c8173-4d74-470f-9f14-d8809a981ce7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
